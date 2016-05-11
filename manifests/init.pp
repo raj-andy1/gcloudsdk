47ccb897040c6cf4daedda7214ee5d491d57a18b
RanjithKumar45 <kumar.sree45@gmail.com>
2016-05-11 11:10:02 +0530

    Fix for summary and indentation fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@
 # Sample Usage: https://github.com/RanjthKumar45/puppet-gcloudsdk#usage
 #
 class gcloudsdk (
-  $version = $::gcloudsdk::params::version, 
+  $version = $::gcloudsdk::params::version,
   $install_dir = $::gcloudsdk::params::install_dir,
   $is_install_gcloud = $::gcloudsdk::params::is_install_gcloud,
   $is_install_gsutil = $::gcloudsdk::params::is_install_gsutil,
@@ -66,24 +66,24 @@ class gcloudsdk (
 
   # The below block of code downloads the google-cloud-sdk archive file.
   archive::download{ "${download_file_name}.tar.gz":
-    url => $download_source,
+    url        => $download_source,
     src_target => '/tmp',
-    checksum => false,
+    checksum   => false,
   }
   
   # The below block of code extracts the google-cloud-sdk archive file.
   archive::extract { $download_file_name:
-    target  => $install_dir,
-    src_target => "/tmp",
-    require => Archive::Download["${download_file_name}.tar.gz"],
+    target     => $install_dir,
+    src_target =>'/tmp',
+    require    => Archive::Download["${download_file_name}.tar.gz"],
   }
   
   # The below block of code installs the google-cloud-sdk archive file.
   $install_path = "${install_dir}/google-cloud-sdk"
   exec { 'install Google Cloud SDK':
-    path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     creates => "${install_path}/bin/gcloud",
-    cwd => "${install_dir}/google-cloud-sdk",
+    cwd     => "${install_dir}/google-cloud-sdk",
     command => '/bin/echo "" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false',
     require => Archive::Extract[$download_file_name],
   }
@@ -91,52 +91,52 @@ class gcloudsdk (
   
   
   # The below code will set the google-cloud-sdk path inside the PATH env variable.
-  file { "/etc/profile.d/gcloud_path.sh":
+  file { '/etc/profile.d/gcloud_path.sh':
     ensure  => file,
-    mode => '0755',
+    mode    => '0755',
     content => template('gcloudsdk/gcloud_path.sh.erb'),
-    require => Archive::Extract["${download_file_name}"],
+    require => Archive::Extract[$download_file_name],
   }-> exec { 'set gcloud':
     provider  => shell,
-    command   => "sh /etc/profile.d/gcloud_path.sh",
+    command   => 'sh /etc/profile.d/gcloud_path.sh',
     logoutput => on_failure,
-  }  
+  }
   
   # The below code will install the selected addtional components. (only if any additional components is set to true)
-  file { "/tmp/sdk_add_components.sh":
+  file { '/tmp/sdk_add_components.sh':
     ensure  => file,
-    mode => '0755',
+    mode    => '0755',
     content => template('gcloudsdk/sdk_add_components.sh.erb'),
     require => Exec['set gcloud'],
   }-> exec { 'Add Components':
     provider  => shell,
-    command   => "sh /tmp/sdk_add_components.sh",
+    command   => 'sh /tmp/sdk_add_components.sh',
     logoutput => on_failure,
-  } 
+  }
   
   # The below code will Update the components to latest version. (only if version is set to LATEST)
-  file { "/tmp/sdk_update_components.sh":
+  file { '/tmp/sdk_update_components.sh':
     ensure  => file,
-    mode => '0755',
+    mode    => '0755',
     content => template('gcloudsdk/sdk_update_components.sh.erb'),
     require => Exec['Add Components'],
   }-> exec { 'Update Components':
     provider  => shell,
-    command   => "sh /tmp/sdk_update_components.sh",
+    command   => 'sh /tmp/sdk_update_components.sh',
     logoutput => on_failure,
-  } 
+  }
   
   # The below code will uninstall the selected default components. (only if default components installation is set to false)
-  file { "/tmp/sdk_remove_components.sh":
+  file { '/tmp/sdk_remove_components.sh':
     ensure  => file,
-    mode => '0755',
+    mode    => '0755',
     content => template('gcloudsdk/sdk_remove_components.sh.erb'),
     require => Exec['Update Components'],
   }-> exec { 'Remove Components':
     provider  => shell,
-    command   => "sh /tmp/sdk_remove_components.sh",
+    command   => 'sh /tmp/sdk_remove_components.sh',
     logoutput => on_failure,
-  } 
+  }
     
 }
 
