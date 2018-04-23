bbfa09895410636b7786c88a1e6bbfdb01ab3165
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-04-23 10:51:07 -0700

    remove the |

--- lib/facter/gcloud_version.rb
+++ lib/facter/gcloud_version.rb
@@ -2,6 +2,9 @@
 Facter.add(:gcloud_version) do
     confine :kernel => :'linux'
     setcode do
-        Facter::Core::Execution.execute('/bin/gcloud version --format='json' | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Google Cloud SDK"]'')
+        Facter::Core::Execution.execute('/bin/gcloud version --format='json'')
     end
 end
+
+
+#| /usr/bin/python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Google Cloud SDK"]'
