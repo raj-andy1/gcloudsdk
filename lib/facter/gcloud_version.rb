2932cf31acb412df9ddc48a9ecd86637c9e1a74a
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-04-23 10:55:12 -0700

    change 'json' to json and removed other comments

--- lib/facter/gcloud_version.rb
+++ lib/facter/gcloud_version.rb
@@ -1,10 +1,6 @@
-#custom fact to add gcloud version upton install
 Facter.add(:gcloud_version) do
     confine :kernel => :'linux'
     setcode do
-        Facter::Core::Execution.execute('/bin/gcloud version --format='json'')
+        Facter::Core::Execution.execute('/bin/gcloud version --format=json')
     end
 end
-
-
-#| /usr/bin/python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Google Cloud SDK"]'
