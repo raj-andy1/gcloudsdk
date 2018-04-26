e7b53bc4a0c0ba848a1b685918c05b71d406265d
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-04-25 18:56:39 -0700

    remove result=nil

--- lib/facter/gcloud_version.rb
+++ lib/facter/gcloud_version.rb
@@ -2,7 +2,6 @@ Facter.add(:gcloud_version) do
     confine :kernel => 'linux'
     setcode do
         begin
-            result = nil
             result = Facter::Core::Execution.execute('gcloud version --format=json')
         rescue
             nil
